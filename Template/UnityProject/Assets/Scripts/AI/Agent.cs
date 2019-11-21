ff4fa6ed6320d8d14a7acf027233cd7164e9ec29
Milan Zolota <mizolota@microsoft.com>
2019-11-21 10:04:29 +0100

    split

---
+++ Template/UnityProject/Assets/Scripts/AI/Agent.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class that combines a genotype and a feedforward neural network (FNN).
+/// </summary>
+public class Agent : IComparable<Agent>
+{
+    /// <summary>
+    /// The underlying genotype of this agent.
+    /// </summary>
+    public Genotype Genotype
+    {
+        get;
+    }
+
+    /// <summary>
+    /// The feedforward neural network which was constructed from the genotype of this agent.
+    /// </summary>
+    public NeuralNetwork FNN
+    {
+        get;
+    }
+
+    private bool isAlive;
+    /// <summary>
+    /// Whether this agent is currently alive (actively participating in the simulation).
+    /// </summary>
+    public bool IsAlive
+    {
+        get => isAlive;
+        private set
+        {
+            if (isAlive == value) return;
+            isAlive = value;
+
+            if (!isAlive && AgentDied != null)
+                AgentDied(this);
+        }
+    }
+    /// <summary>
+    /// Event for when the agent died (stopped participating in the simulation).
+    /// </summary>
+    public event Action<Agent> AgentDied;
+
+    /// <summary>
+    /// Initialises a new agent from given genotype, constructing a new feedfoward neural network from
+    /// the parameters of the genotype.
+    /// </summary>
+    /// <param name="genotype">The genotpye to initialise this agent from.</param>
+    /// <param name="topology">The topology of the feedforward neural network to be constructed from given genotype.</param>
+    public Agent(Genotype genotype, params uint[] topology)
+    {
+        IsAlive = false;
+        this.Genotype = genotype;
+        FNN = new NeuralNetwork(topology);
+
+        //Check if topology is valid
+        if (FNN.WeightCount != genotype.ParameterCount)
+            throw new ArgumentException("The given genotype's parameter count must match the neural network topology's weight count.");
+
+        //Construct FNN from genotype
+        IEnumerator<float> parameters = genotype.GetEnumerator();
+        foreach (NeuralLayer layer in FNN.Layers) //Loop over all layers
+        {
+            for (int i = 0; i < layer.Weights.GetLength(0); i++) //Loop over all nodes of current layer
+            {
+                for (int j = 0; j < layer.Weights.GetLength(1); j++) //Loop over all nodes of next layer
+                {
+                    layer.Weights[i,j] = parameters.Current;
+                    parameters.MoveNext();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Resets this agent to be alive again.
+    /// </summary>
+    public void Reset()
+    {
+        Genotype.Evaluation = 0;
+        Genotype.Fitness = 0;
+        IsAlive = true;
+    }
+
+    /// <summary>
+    /// Kills this agent (sets IsAlive to false).
+    /// </summary>
+    public void Kill()
+    {
+        IsAlive = false;
+    }
+
+    /// <summary>
+    /// Compares this agent to another agent, by comparing their underlying genotypes.
+    /// </summary>
+    /// <param name="other">The agent to compare this agent to.</param>
+    /// <returns>The result of comparing the underlying genotypes of this agent and the given agent.</returns>
+    public int CompareTo(Agent other)
+    {
+        return this.Genotype.CompareTo(other.Genotype);
+    }
+}
+
---
+++ Template/UnityProject/Assets/Scripts/AI/NeuralNetworks/NeuralLayer.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+
+/// <summary>
+/// Class representing a single layer of a fully connected feedforward neural network.
+/// </summary>
+public class NeuralLayer
+{
+    /// <summary>
+    /// Delegate representing the activation function of an artificial neuron.
+    /// </summary>
+    /// <param name="xValue">The input value of the function.</param>
+    /// <returns>The calculated output value of the function.</returns>
+    public delegate double ActivationFunction(double xValue);
+
+    /// <summary>
+    /// The amount of neurons in this layer.
+    /// </summary>
+    public uint NeuronCount
+    {
+        get;
+    }
+
+    /// <summary>
+    /// The amount of neurons this layer is connected to, i.e., the amount of neurons of the next layer.
+    /// </summary>
+    public uint OutputCount
+    {
+        get;
+    }
+
+    public double[,] Weights
+    {
+        get;
+        set;
+    }
+
+    public NeuralLayer(uint nodeCount, uint outputCount)
+    {
+        this.NeuronCount = nodeCount;
+        this.OutputCount = outputCount;
+
+        Weights = new double[nodeCount + 1, outputCount]; // + 1 for bias node
+    }
+
+    /// <summary>
+    /// Processes the given inputs using the current weights to the next layer.
+    /// </summary>
+    /// <param name="inputs">The inputs to be processed.</param>
+    /// <returns>The calculated outputs.</returns>
+    public double[] ProcessInputs(double[] inputs)
+    {
+        if (inputs.Length != NeuronCount)
+            throw new Exception("Wrong inputs in NL");
+
+        var output = new double[OutputCount];
+        var biasedInput = new double[NeuronCount + 1];
+        inputs.CopyTo(biasedInput, 0);
+        biasedInput[biasedInput.Length - 1] = 1;
+
+        for (var i = 0; i < OutputCount; i++)
+        {
+            for (var j = 0; j < NeuronCount; j++)
+            {
+                output[i] += inputs[j] * Weights[j, i];
+            }
+        }
+
+        //Apply activation function to sum, if set
+        for (var i = 0; i < output.Length; i++)
+            output[i] = MathHelper.SoftSignFunction(output[i]);
+
+        return output;
+    }
+}
